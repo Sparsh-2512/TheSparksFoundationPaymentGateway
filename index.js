6f544b3417e181debb77c6ba5283c0f363b4d9c7
Sparsh-2512 <sparshkhandelwal729cd@gmail.com>
2021-01-19 16:13:09 +0530

    made some design changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // for changing quotes
 let i = 1;
-let quotesContainer = document.getElementById('quotesContainer');
-let quotes =
-    [
-        "“No one has ever become poor by giving.” ―Anne Frank", "“Remember that the happiest people are not those getting more, but those giving more.” ―H. Jackson Brown Jr.", "“We must give more in order to get more. It is the generous giving of ourselves that produces the generous harvest.” ―Orison Swett Marden", "“We make a living by what we get. We make a life by what we give.” ―Winston S. Churchill", "“No one is useless in this world who lightens the burdens of another.” ―Charles Dickens", "“For it is in giving that we receive.” ―St. Francis of Assisi"
-    ];
 setInterval(() => {
-    if (i === quotes.length)
-        i = 0;
-    quotesContainer.innerText = quotes[i++];
+    let pre = i-1;
+    let quote = document.getElementById(`quote${i++}`);
+    if (i === 7)
+        i = 1;
+    if (pre === 0)
+        pre = 6;
+    let preQuote = document.getElementById(`quote${pre}`);
+    preQuote.style.display = 'none';
+    quote.style.display = 'inline-block';
 }, 6000);
